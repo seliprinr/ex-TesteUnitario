03959547a64bff9b664af80a91c39acf348fcfde
Rodrigo Seliprin <rseliprin@gmail.com>
2023-04-10 05:54:21 -0300

    Teste unitário em Kotlin

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -7,7 +7,7 @@ class MainTest
     @Test
     @DisplayName("Testa os cenarios da portaria")
     fun testPortaria() {
-    Assertions.assertEquals(portaria(idade = 15, tpConvite = "", cod = ""), actual = "Negado.")
+    Assertions.assertEquals(portaria(idade = 20, tpConvite = "", cod = ""), actual = "Negado.")
     }
 
 }
